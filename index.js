c228f4935dfdcf34993de765209b2f411dd0012b
christiansallen <christiansallen@gmail.com>
2019-02-26 16:16:32 -0700

    Added cookies/sessions and logout functionality

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const helmet = require("helmet");
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
 const db = require("./database/dbConfig.js");
-const Users = require("./users/users-module.js");
+const Users = require("./users/users-model.js");
 
 const server = express();
 
+const sessionConfig = {
+  name: "cookies!",
+  secret: "this is my secret",
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+    secure: false
+  },
+  httpOnly: true,
+  reSave: false,
+  saveUninitialized: false,
+
+  store: new KnexSessionStore({
+    knex: db,
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60 * 24
+  })
+};
+
 server.use(helmet());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 const port = 5000;
 server.listen(port, () => {
@@ -32,11 +55,12 @@ server.post("/api/register", (req, res) => {
 server.post("/api/login", (req, res) => {
   let { username, password } = req.body;
 
-  Users.findBy({ username })
+  Users.findBy({ username: username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: "Logged In!" });
+        req.session.user = user;
+        res.status(200).json({ message: "Logged In! + a cookie!" });
       } else {
         res.status(401).json({ message: "You shall not pass!" });
       }
@@ -45,23 +69,31 @@ server.post("/api/login", (req, res) => {
 });
 
 const restricted = (req, res, next) => {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "You shall not pass!" });
-        }
-      })
-      .catch(() => res.status(500).json({ message: "Error in middleware" }));
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(401).json({ message: "No credentials given" });
+    res.status(401).json({ message: "You shall not pass!" });
   }
 };
+
+// const restricted = (req, res, next) => {
+//   const { username, password } = req.headers;
+
+//   if (username && password) {
+//     Users.findBy({ username })
+//       .first()
+//       .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           next();
+//         } else {
+//           res.status(401).json({ message: "You shall not pass!" });
+//         }
+//       })
+//       .catch(() => res.status(500).json({ message: "Error in middleware" }));
+//   } else {
+//     res.status(401).json({ message: "No credentials given" });
+//   }
+// };
 
 server.get("/api/users", restricted, (req, res) => {
   Users.find()
@@ -69,4 +101,18 @@ server.get("/api/users", restricted, (req, res) => {
       res.status(200).json(user);
     })
     .catch(() => res.status(500).json({ message: "Server error" }));
+});
+
+server.get("/api/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(400).json({ message: "Session destroy error" });
+      } else {
+        res.status(200).json({ message: "BYE! You are now logged out!" });
+      }
+    });
+  } else {
+    res.end();
+  }
 });
